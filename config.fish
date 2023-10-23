2a504ef0376b034546db012e690a7b661ef165ea
mason <mason@toss.im>
2023-10-23 18:20:48 +0900

    add fzf plugin

--- config.fish
+++ config.fish
@@ -2,3 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 source ~/.iterm2_shell_integration.fish
+fzf_configure_bindings --directory=\ct
+set fzf_fd_opts --type d -d 4 --hidden --exclude=.git
